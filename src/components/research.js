7d943eee8f5ce4814b067428b9f5947d58aa9de6
mrland99 <max.ruikang.land@gmail.com>
2020-06-15 21:59:27 -0400

    update style

--- src/components/research.js
+++ src/components/research.js
@@ -3,6 +3,7 @@ import '../styles/layout.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import spatial from '../images/spatial.png';
+import coming_soon from '../images/coming_soon.png';
 
 function Research() {
 
@@ -14,24 +15,39 @@ function Research() {
             </div>
 
             <div class="spotlight-divider"></div>
-            <div class="spotlight">
-                <div class="proj-image">
-                    <img  src={spatial} alt="EasyGov Logo" />
+                <div class="spotlight">
+                    <div class="proj-image">
+                        <img  src={coming_soon} alt="EasyGov Logo" />
+                    </div>
+                    <div class="proj-content">
+                        <h3>Tissue Alignment via Optimal Transport
+                        </h3>
+                        <br></br>
+                        <h4>Raphael Lab, Summer 2020</h4>
+                        <br></br>
+                        <p>Currently developing a method based in Optimal Tranport Theory that utilizes both spatial and gene expression data in order to align
+                            separate tissue layers.
+                        </p>
+                    </div>
                 </div>
-                <div class="proj-content">
-                    <h3>Determining Cancerous Regions in Breast Cancer Tissue <a href="https://github.com/mrland99/spatial-research" class="proj-link">
-                        <FontAwesomeIcon icon={faLink}></FontAwesomeIcon></a>
-                    </h3>
-                    <br></br>
-                    <h4>Raphael Lab, Spring 2020</h4>
-                    <br></br>
-                    <p>Developed two novel methods to distinguish between normal cells and cancer cells
-                        without requiring prior characterization and establishment of gene expression profiles of varioustypes of cancer cells.  
-                        Instead, these methods consider the entire transcriptome as a whole and use PCA and percent dropout to reduce our data
-                         to one dimensional feature vectors.
-                    </p>
+                <div class="spotlight">
+                    <div class="proj-image">
+                        <img  src={spatial} alt="Spatial Tissue" />
+                    </div>
+                    <div class="proj-content">
+                        <h3>Determining Cancerous Regions in Breast Cancer Tissue <a href="https://github.com/mrland99/spatial-research" class="proj-link">
+                            <FontAwesomeIcon icon={faLink}></FontAwesomeIcon></a>
+                        </h3>
+                        <br></br>
+                        <h4>Raphael Lab, Spring 2020</h4>
+                        <br></br>
+                        <p>Developed two novel methods to distinguish between normal cells and cancer cells
+                            without requiring prior characterization and establishment of gene expression profiles of varioustypes of cancer cells.  
+                            Instead, these methods consider the entire transcriptome as a whole and use PCA and percent dropout to reduce our data
+                            to one dimensional feature vectors.
+                        </p>
+                    </div>
                 </div>
-            </div>
          </div>
     </div>
   );
