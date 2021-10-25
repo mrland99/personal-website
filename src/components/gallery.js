14ba438344dec2c7e3ca76c9ea64aa1993390033
mrland99 <max.ruikang.land@gmail.com>
2021-10-24 22:39:21 -0400

    add two gallery images

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -9,6 +9,7 @@ import glacier_point_sunset from '../images/gallery-images/glacier_point_sunset.
 import john_muirs_trail from '../images/gallery-images/john_muirs_trail.jpg';
 import sur from '../images/gallery-images/sur.jpg';
 import golden_gate2 from '../images/gallery-images/golden_gate2.jpg';
+import marigold_bee from '../images/gallery-images/bronx_marigold_bee.jpg'
 
 /* Landscape Images */
 import golden_gate1 from '../images/gallery-images/golden_gate1.jpg'
@@ -19,6 +20,7 @@ import thomas_lab from '../images/gallery-images/thomas_lab.jpg'
 import old_city from '../images/gallery-images/old_city.jpg'
 import blair from '../images/gallery-images/blair.jpg'
 import richardson from '../images/gallery-images/richardson.jpg'
+import sams_point from '../images/gallery-images/sams_point.jpg'
 
 class Gallery extends Component {
     componentDidMount() {
@@ -34,6 +36,16 @@ class Gallery extends Component {
                 </Helmet>
                 <p className="heading"> Landscape Gallery</p>
                 <div className="gallery-image-landscape">    
+                    <div className="img-box-landscape">
+                        <img src={sams_point} alt="" />
+                        <div className="transparent-box-landscape">
+                        <div className="caption">
+                            <p>Sam's Point</p>
+                            <p className="opacity-low">Shot on Pxiel 5 -- with Brandy Chen</p>
+                        </div>
+                        </div> 
+                    </div>
+
                     <div className="img-box-landscape">
                         <img src={maroon_bells} alt="" />
                         <div className="transparent-box-landscape">
@@ -59,7 +71,7 @@ class Gallery extends Component {
                         <div className="transparent-box-landscape">
                         <div className="caption">
                             <p>Glacier Point</p>
-                            <p className="opacity-low">Shot on Pixel -- with Brandy Chen</p>
+                            <p className="opacity-low">Shot on Pixel 4a -- with Brandy Chen</p>
                         </div>
                         </div> 
                     </div>
@@ -69,7 +81,7 @@ class Gallery extends Component {
                         <div className="transparent-box-landscape">
                         <div className="caption">
                             <p>Half Dome</p>
-                            <p className="opacity-low">Shot on Pixel -- with Brandy Chen</p>
+                            <p className="opacity-low">Shot on Pixel 4a -- with Brandy Chen</p>
                         </div>
                         </div>
                     </div>
@@ -79,7 +91,7 @@ class Gallery extends Component {
                         <div className="transparent-box-landscape">
                         <div className="caption">
                             <p>Bixby Creek Bridge</p>
-                            <p className="opacity-low">Shot on Pixel -- with Brandy Chen</p>
+                            <p className="opacity-low">Shot on Pixel 4a -- with Brandy Chen</p>
                         </div>
                         </div>
                     </div>
@@ -97,7 +109,17 @@ class Gallery extends Component {
                 </div>
          
                 <p className="heading"> Portrait Gallery</p>
-                <div className="gallery-image-portrait">  
+                <div className="gallery-image-portrait"> 
+                    <div className="img-box-portrait">
+                        <img src={marigold_bee} alt="" />
+                        <div className="transparent-box-portrait">
+                        <div className="caption">
+                            <p>Native NY Bee x Marigolds</p>
+                            <p className="opacity-low">Shot on Pixel 4a</p>
+                        </div>
+                        </div> 
+                    </div>
+
                     <div className="img-box-portrait">
                         <img src={golden_gate1} alt="" />
                         <div className="transparent-box-portrait">
